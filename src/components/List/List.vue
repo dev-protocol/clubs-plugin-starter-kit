b05c48cc39935b676673fa77f7b9c1c443dd85ca
kazuyoshi80 <kkawakami@smartshopping.co.jp>
2022-09-22 08:12:27 +0900

    Make it a Composition API | List.vue

--- src/components/List/List.vue
+++ src/components/List/List.vue
@@ -1,27 +1,26 @@
+<script lang="ts" setup>
+import type { PropType } from 'vue'
+import type { TokenURIWithId } from '../../types'
+import Image from '../Image/Image.vue'
+
+const props = defineProps({
+	slug: String,
+	tokens: {
+		type: Object as PropType<TokenURIWithId[]>,
+		required: true,
+	},
+})
+
+return { slug: props.slug, tokens: props.tokens }
+</script>
+
 <template>
 	<ul class="grid gap-2">
 		<li v-for="token in tokens" v-bind:key="token.id">
-			<a :href="slug + '/' + token.id" class="flex gap-2"
-				><span>#{{ token.id }}</span
-				><Image :src="token.image" class="h-8"
-			/></a>
+			<a :href="`${slug}/${token.id}`" class="flex gap-2">
+					<span>#{{ token.id }}</span>
+					<Image :src="token.image" class="h-8" />
+			</a>
 		</li>
 	</ul>
 </template>
-
-<script lang="ts">
-import type { TokenURIWithId } from '../../types'
-import { defineComponent, PropType } from 'vue'
-import Image from '../Image/Image.vue'
-
-export default defineComponent({
-	components: { Image },
-	props: {
-		slug: String,
-		tokens: {
-			type: Array as PropType<TokenURIWithId[]>,
-			required: true,
-		},
-	},
-})
-</script>
