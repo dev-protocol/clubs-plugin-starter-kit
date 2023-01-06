057af7d6f77a04af6f667de9daa26e749c0bcb60
aggre <hiroyuki.aggre@gmail.com>
2023-01-06 13:14:17 +0900

    change build structures

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import { dirname, relative, resolve } from 'path'
+import typescript from '@rollup/plugin-typescript'
+
+const dir = 'dist'
+
+const useSrc = ({ dir, ext } = {}) => ({
+	name: 'local:useSrc',
+	resolveId(source, importer) {
+		if (ext.some((e) => source.endsWith(e))) {
+			const from = dir ?? dirname(importer)
+			const importerDir = dirname(importer)
+			const original = resolve(importerDir, source)
+			const relativePath = relative(from, original)
+			return {
+				id: relativePath,
+				external: true,
+			}
+		}
+	},
+})
+
+export default [
+	{
+		input: 'src/index.ts',
+		output: [
+			{
+				dir,
+				format: 'es',
+			},
+		],
+		plugins: [
+			typescript(),
+			useSrc({ ext: ['.astro', '.svelte', '.vue', '.scss', '.css'], dir }),
+		],
+	},
+]
