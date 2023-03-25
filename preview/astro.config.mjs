e7d989d26accc7e03eed82c819136baf12652620
Kazuyoshi Kawakami <saruyama.monki@gmail.com>
2023-03-25 16:17:28 +0900

    Add astro/svelte package

--- preview/astro.config.mjs
+++ preview/astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config'
 import clubs from '@devprotocol/clubs-core'
 import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind'
+import svelte from '@astrojs/svelte'
 
 export default defineConfig({
 	server: {
 		port: 3000,
 	},
-	integrations: [clubs(), vue(), tailwind()],
+	integrations: [clubs(), vue(), tailwind(), svelte()],
 })
