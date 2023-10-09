a13ae32fbe27ef91ed6c7842679b156760da1bb6
aggre <hiroyuki.aggre@gmail.com>
2023-10-10 00:29:35 +0900

    follow the latest structure

--- .preview/preview.static/tailwind.config.mjs
+++
@@ -1,5 +0,0 @@
-import { clubs } from '@devprotocol/clubs-core/tailwind'
-
-export default {
-	presets: [clubs],
-}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
 import { clubs } from '@devprotocol/clubs-core/tailwind'
+import typography from '@tailwindcss/typography'
 
 export default {
 	presets: [clubs],
+	plugins: [typography],
 }
