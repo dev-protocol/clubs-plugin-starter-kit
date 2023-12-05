01c47a98534048117d720599337b8dec515e153a
aggre <hiroyuki.aggre@gmail.com>
2023-12-05 17:20:38 +0900

    impr existing tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,19 @@
 import { describe, it, expect } from 'vitest'
-import plugin from './index'
+import plugin, { getPagePaths, getAdminPaths, meta } from './index'
+
+describe('plugin default export', () => {
+	it('should have getPagePaths, getAdminPaths and meta', async () => {
+		expect(plugin).toEqual({
+			getPagePaths,
+			getAdminPaths,
+			meta,
+		})
+	})
+})
 
 describe('getPagePaths', () => {
 	it('should return empty array if options are not set', async () => {
-		const res = await plugin.getPagePaths([])
+		const res = await getPagePaths([])
 		expect(res).toEqual([])
 	})
 })
