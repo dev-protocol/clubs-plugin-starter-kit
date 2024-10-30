c51ad844aa7bf596c16ab1949d828e7e660a5a17
Aggre <hiroyuki.aggre@gmail.com>
2024-10-30 09:59:53 +0900

    upgrade

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
 import { dirname, relative, resolve } from 'path'
 import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import { cwd } from 'process'
 
 const dir = 'dist'
 
-const useSrc = ({ dir: _dir, ext } = {}) => ({
+const useSrc = ({ out, ext } = {}) => ({
 	name: 'local:useSrc',
 	resolveId(source, importer) {
 		if (ext.some((e) => source.endsWith(e))) {
-			const from = _dir ?? dirname(importer)
-			const importerDir = dirname(importer)
-			const original = resolve(importerDir, source)
+			const here = cwd()
+			const from =
+				typeof out === 'string'
+					? out
+					: dirname(typeof out === 'function' ? out(importer) : importer)
+			const originalImporter = importer.replace(`${here}/dist`, here)
+			const originalImporterDir = dirname(originalImporter)
+			const original = resolve(originalImporterDir, source)
 			const relativePath = relative(from, original)
 			return {
 				id: relativePath,
@@ -30,7 +37,23 @@ export default [
 		],
 		plugins: [
 			typescript(),
-			useSrc({ ext: ['.astro', '.svelte', '.vue', '.scss', '.css'], dir }),
+			useSrc({
+				ext: ['.astro', '.svelte', '.vue', '.scss', '.css'],
+				dir,
+				out: (path) => path.replace('src', 'dist'),
+			}),
+		],
+	},
+	{
+		input: 'dist/src/index.d.ts',
+		output: [{ file: 'dist/index.d.ts', format: 'es' }],
+		plugins: [
+			dts(),
+			useSrc({
+				ext: ['.astro', '.svelte', '.vue', '.scss', '.css', '.png', '.jpg'],
+				dir,
+				out: (path) => path.replace('dist/src', 'dist'),
+			}),
 		],
 	},
 ]
