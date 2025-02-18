e8f3c485d819e951f29f6b3f6edea38c83204a42
Aggre <hiroyuki.aggre@gmail.com>
2025-02-18 11:44:54 +0900

    upgrade the latest astro ecosystem

--- .preview/preview.ssr/astro.config.mjs
+++ .preview/preview.ssr/astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config'
 import clubs from '@devprotocol/clubs-core'
 import vue from '@astrojs/vue'
 import svelte from '@astrojs/svelte'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import { config } from 'dotenv'
 
 config({ path: './.env' })
 
 export default defineConfig({
 	output: 'server',
-	integrations: [clubs(), vue(), svelte(), tailwind()],
+	integrations: [clubs(), vue(), svelte()],
+	vite: { plugins: [tailwindcss()] },
 })
--- .preview/preview.static/astro.config.mjs
+++ .preview/preview.static/astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config'
 import clubs from '@devprotocol/clubs-core'
 import vue from '@astrojs/vue'
 import svelte from '@astrojs/svelte'
-import tailwind from '@astrojs/tailwind'
+import tailwindcss from '@tailwindcss/vite'
 import { config } from 'dotenv'
 
 config({ path: './.env' })
 
 export default defineConfig({
-	integrations: [clubs(), vue(), svelte(),tailwind()],
+	integrations: [clubs(), vue(), svelte()],
+	vite: { plugins: [tailwindcss()] },
 })
