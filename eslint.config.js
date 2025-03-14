589f9f76ab78293a8ac30d74165cbac3065a1475
Aggre <hiroyuki.aggre@gmail.com>
2025-03-14 12:09:52 +0900

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -33,5 +33,5 @@ export default tseslint.config(
 			'@typescript-eslint/no-explicit-any': 'off',
 		},
 	},
-	{ ignores: ['dist', '.yarn', '.preview'] },
+	{ ignores: ['dist', '.yarn', '.preview', '.astro'] },
 )
